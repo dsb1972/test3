13e636647bd25a052e6fd4b5b8afef4130df296a
Suresh <76044527+dsb1972@users.noreply.github.com>
2022-05-31 10:08:43 +0530

    Update three.sh

--- three.sh
+++ three.sh
@@ -3,5 +3,6 @@
 NAME=Suresh
 LASTNAME=Babu
 C=1
-echo "Hello $NAME $LASTNAME $C. Current date is $(date)"
-echo "Hello $NAME $LASTNAME Git-test3 Current date is $(date)" > /tmp/out.txt
+echo "Hello $NAME $LASTNAME $C. Current date is $(date)" >> /tmp/out.txt
+echo "Hello $NAME $LASTNAME Git-test3 Current date is $(date)" >> /tmp/out.txt
+
